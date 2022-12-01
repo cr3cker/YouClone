f344e6e8b6093291a3fbce4382d343c393c849a6
cr3cker <vladstar1606@mail.ru>
2022-12-01 21:24:13 +0300

    Edit templates

--- video_hosting/admin.py
+++ video_hosting/admin.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from .models import Video
 
 
-class VideoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'create_at')
-
 admin.site.register(Video)
