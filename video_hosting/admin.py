abe975849dd7473ba1e1e25aae815eb9487e34af
cr3cker <vladstar1606@mail.ru>
2022-12-01 23:19:08 +0300

    Add views to models

--- video_hosting/admin.py
+++ video_hosting/admin.py
@@ -4,11 +4,16 @@ from .models import Video
 
 class VideoAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Title', {'fields': ['title']}),
-        ('Description', {'fields': ['description']}),
-        ('Video Preview', {'fields': ['image']}),
-        ('Video File (mp4)', {'fields': ['file']}),
+        ('Title and Description', {
+            'fields': ('title', 'description')
+        }),
+        ('Preview and Video', {
+            'classes': ('collapse',),
+            'fields': ('image', 'file'),
+        }),
     ]
+    date_hierarchy = 'create_at'
+    empty_value_display = '-empty-'
     list_display = ('title', 'file', 'create_at')
     list_filter = ['create_at']
     search_fields = ('title', 'file', 'image', 'create_at')
--- video_hosting/models.py
+++ video_hosting/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import FileExtensionValidator
 from django.db import models
+import random
 
 
 class Video(models.Model):
@@ -11,6 +12,13 @@ class Video(models.Model):
         validators=[FileExtensionValidator(allowed_extensions=['mp4'])]
     )
     create_at = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=random.randint(500, 10000))
+
+    class Meta:
+        verbose_name = 'Video'
+        verbose_name_plural = 'Videos'
+        ordering = ['-create_at']
+
 
     def __str__(self):
         return self.title
